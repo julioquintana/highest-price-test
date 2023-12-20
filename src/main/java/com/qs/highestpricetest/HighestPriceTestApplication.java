37c34a5fa2082cd5f159bc90dde5915f6b93f67e
JulioQuintana <juliocesarquintanasilva@gmail.com>
2023-12-20 17:58:12 -0300

    Add spring project
    I added the spring project configuration

---
+++ src/main/java/com/qs/highestpricetest/HighestPriceTestApplication.java
@@ -0,0 +1,13 @@
+package com.qs.highestpricetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighestPriceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighestPriceTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qs/highestpricetest/HighestPriceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qs.highestpricetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighestPriceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
