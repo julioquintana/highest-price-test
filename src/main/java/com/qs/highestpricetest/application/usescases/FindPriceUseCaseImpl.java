f994a18d91eb4894dc708aa1643ebb631fdcc323
JulioQuintana <juliocesarquintanasilva@gmail.com>
2023-12-21 14:02:40 -0300

    Little fix

--- src/main/java/com/qs/highestpricetest/application/usescases/FindPriceUseCaseImpl.java
+++ src/main/java/com/qs/highestpricetest/application/usescases/FindPriceUseCaseImpl.java
@@ -1,11 +1,10 @@
 package com.qs.highestpricetest.application.usescases;
 
 import com.qs.highestpricetest.application.exception.NotFoundException;
+import com.qs.highestpricetest.domain.model.PriceDto;
 import com.qs.highestpricetest.domain.port.in.FindPriceUseCase;
 import com.qs.highestpricetest.domain.port.out.PriceRepositoryPort;
-import com.qs.highestpricetest.domain.model.PriceDto;
 import lombok.AllArgsConstructor;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
