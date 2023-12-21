e0324252e4e7bbdbbbdcfa9f2088f68f2838c3ae
JulioQuintana <juliocesarquintanasilva@gmail.com>
2023-12-21 13:54:35 -0300

    Change to mono response

--- src/main/java/com/qs/highestpricetest/infrastructure/adapter/in/rest/PriceController.java
+++ src/main/java/com/qs/highestpricetest/infrastructure/adapter/in/rest/PriceController.java
@@ -1,13 +1,13 @@
 package com.qs.highestpricetest.infrastructure.adapter.in.rest;
 
 import com.qs.highestpricetest.application.services.PriceCommand;
-import com.qs.highestpricetest.infrastructure.adapter.in.model.PriceDto;
+import com.qs.highestpricetest.domain.model.PriceDto;
 import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 
@@ -17,11 +17,10 @@ public class PriceController {
 
     private final PriceCommand priceCommand;
     @GetMapping(path = "/api/v1/price")
-    public Flux<PriceDto> findPrice(
+    public Mono<PriceDto> findPrice(
             @RequestParam(name = "brandID") Integer brandID,
             @RequestParam(name = "productID") Integer productID,
             @RequestParam(name = "purchaseDay") @DateTimeFormat(pattern = "yyyy-MM-dd-HH.mm.ss") LocalDateTime purchaseDay) {
-
-        return priceCommand.find(brandID,productID, purchaseDay);
+        return priceCommand.find(brandID, productID, purchaseDay);
     }
 }
