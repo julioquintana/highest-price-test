affd9f859e4dc9bf7a39e2947527ce319be01ae3
JulioQuintana <juliocesarquintanasilva@gmail.com>
2023-12-21 17:44:08 -0300

    optimize import

--- src/main/java/com/qs/highestpricetest/infrastructure/repositories/ReactivePriceRepository.java
+++ src/main/java/com/qs/highestpricetest/infrastructure/repositories/ReactivePriceRepository.java
@@ -13,10 +13,10 @@ import java.time.LocalDateTime;
 public interface ReactivePriceRepository extends ReactiveCrudRepository<PriceEntity, Long> {
 
     @Query("SELECT * FROM PRICES p "
-            + "WHERE p.BRAND_ID = :brandID AND "
-            + "p.PRODUCT_ID = :productID AND "
-            + "(:purchaseDay >= p.START_DATE AND :purchaseDay <= p.END_DATE) ORDER BY p.PRIORITY DESC "
-            + "LIMIT 1;"
+           + "WHERE p.BRAND_ID = :brandID AND "
+           + "p.PRODUCT_ID = :productID AND "
+           + "(:purchaseDay >= p.START_DATE AND :purchaseDay <= p.END_DATE) ORDER BY p.PRIORITY DESC "
+           + "LIMIT 1;"
     )
     Mono<PriceEntity> findHighestPriceByBrandAndProductAndDate(@Param("brandID") Integer brandID,
                                                                @Param("productID") Integer productID,
