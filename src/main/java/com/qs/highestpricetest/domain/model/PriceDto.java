c30113ed69fa1be784ba8052822b8e6bf3e84529
JulioQuintana <juliocesarquintanasilva@gmail.com>
2023-12-21 13:49:35 -0300

    Create PriceEntity ana PriceDto

---
+++ src/main/java/com/qs/highestpricetest/domain/model/PriceDto.java
@@ -0,0 +1,25 @@
+package com.qs.highestpricetest.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class PriceDto {
+    private Integer id;
+    private Long brandID;
+    @JsonFormat(pattern = "yyyy-MM-dd-HH.mm.ss")
+    private LocalDateTime startDate;
+    @JsonFormat(pattern = "yyyy-MM-dd-HH.mm.ss")
+    private LocalDateTime endDate;
+    private Long priceList;
+    private Long productID;
+    private Long priority;
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER_FLOAT, pattern = "#,##0.00")
+    private BigDecimal price;
+    private String curr;
+}
---
+++ src/main/java/com/qs/highestpricetest/infrastructure/entities/PriceEntity.java
@@ -0,0 +1,71 @@
+package com.qs.highestpricetest.infrastructure.entities;
+
+import com.qs.highestpricetest.domain.model.PriceDto;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("PRICES")
+public class PriceEntity {
+
+    // Price identifier.
+    @Id
+    @Column( "ID")
+    private Integer id;
+
+    // Brand identifier.
+    @Column( "BRAND_ID")
+    private Long brandID;
+
+    // init Range of dates for which the indicated tariff price applies.
+    @Column("START_DATE")
+    private LocalDateTime startDate;
+
+    // finish Range of dates for which the indicated tariff price applies.
+    @Column("END_DATE")
+    private LocalDateTime endDate;
+
+    // Identifier code for the price list.
+    @Column("PRICE_LIST")
+    private Long priceList;
+
+    // Product identifier code.
+    @Column("PRODUCT_ID")
+    private Long productID;
+
+    // Price application disambiguator.
+    // If two tariffs coincide in a date range,
+    // the one with higher priority (higher numerical value) applies.
+    @Column("PRIORITY")
+    private Long priority;
+
+    // Final sale price.
+    @Column("PRICE")
+    private BigDecimal price;
+
+    // ISO code of the currency.
+    @Column("CURR")
+    private String curr;
+
+
+    public PriceDto toDomainModel() {
+        return new PriceDto(getId(),
+                getBrandID(),
+                getStartDate(),
+                getEndDate(),
+                getPriceList(),
+                getProductID(),
+                getPriority(),
+                getPrice(),
+                getCurr());
+    }
+}
